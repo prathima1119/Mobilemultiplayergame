f3356e0b1fb1530bb2ae82949df9883e4874cb8f
SaiPrabhu611 <41958569+SaiPrabhu611@users.noreply.github.com>
2019-08-19 23:19:58 +0530

    24

--- MobileMultiplayer/Assets/SCRIPTS/PlayerMovementController.cs
+++ MobileMultiplayer/Assets/SCRIPTS/PlayerMovementController.cs
@@ -10,10 +10,14 @@ public class PlayerMovementController : MonoBehaviour
 
 
     private RigidbodyFirstPersonController rigidBodyController;
+
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidBodyController = GetComponent < RigidbodyFirstPersonController >();   
+        rigidBodyController = GetComponent < RigidbodyFirstPersonController >();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,5 +31,25 @@ public class PlayerMovementController : MonoBehaviour
         rigidBodyController.joystickInputAxis.x = joyStick.Horizontal;
         rigidBodyController.joystickInputAxis.y = joyStick.Vertical;
         rigidBodyController.mouseLook.lookInputAxis = fixedTouchField.TouchDist;
+
+        Debug.Log("Horizontal" + joyStick.Horizontal);
+        Debug.Log("Vertical" + joyStick.Vertical);
+
+        animator.SetFloat("Horizontal", joyStick.Horizontal);
+        animator.SetFloat("Vertical", joyStick.Vertical);
+
+        if(Mathf.Abs(joyStick.Horizontal)>0.9f || Mathf.Abs(joyStick.Vertical)>0.9f)
+        {
+            //running
+            rigidBodyController.movementSettings.ForwardSpeed = 16;
+            animator.SetBool("Horizontal", true);
+
+        }
+        else
+        {
+            //not running
+            rigidBodyController.movementSettings.ForwardSpeed = 8;
+            animator.SetBool("Horizontal", false);
+        }
     }
 }
