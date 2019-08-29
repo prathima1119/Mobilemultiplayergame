616701590c41e0308037932c6fe66c6926b8497c
prathima <rameshchigullapally977@gmail.com>
2019-08-28 23:43:54 -0700

    game completed

--- MobileMultiplayer/Assets/SCRIPTS/Shooting.cs
+++ MobileMultiplayer/Assets/SCRIPTS/Shooting.cs
@@ -27,21 +27,21 @@ public class Shooting : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Fire()
     {
         RaycastHit _hit;
-        Ray ray = FPS_Camera.ViewportPointToRay(new Vector3(0.5f,0.5f));
-        if(Physics.Raycast(ray,out _hit,100))
+        Ray ray = FPS_Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        if (Physics.Raycast(ray, out _hit, 100))
         {
             Debug.Log(_hit.collider.gameObject.name);
 
-            photonView.RPC("CreateHitEffect",RpcTarget.All,_hit.point);
-            if(_hit.collider.gameObject.CompareTag("Player")&&!_hit.collider.gameObject.GetComponent<PhotonView>().IsMine)
+            photonView.RPC("CreateHitEffect", RpcTarget.All, _hit.point);
+            if (_hit.collider.gameObject.CompareTag("Player") && !_hit.collider.gameObject.GetComponent<PhotonView>().IsMine)
             {
-                _hit.collider.gameObject.GetComponent<PhotonView>().RPC("TakeDamage",RpcTarget.AllBuffered,10f);
+                _hit.collider.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.AllBuffered, 10f);
 
 
             }
@@ -50,17 +50,17 @@ public class Shooting : MonoBehaviourPunCallbacks
 
     [PunRPC]
 
-    public void TakeDamage(float _damage,PhotonMessageInfo info)
+    public void TakeDamage(float _damage, PhotonMessageInfo info)
     {
         health -= _damage;
         Debug.Log(health);
 
         healthBar.fillAmount = health / startHealth;
 
-        if(health<=0f)
+        if (health <= 0f)
         {
             Die();
-            Debug.Log(info.Sender.NickName+"killed"+info.photonView.Owner.NickName);
+            Debug.Log(info.Sender.NickName + "killed" + info.photonView.Owner.NickName);
         }
     }
     [PunRPC]
@@ -72,9 +72,41 @@ public class Shooting : MonoBehaviourPunCallbacks
 
     void Die()
     {
-        if(photonView.IsMine)
+        if (photonView.IsMine)
         {
-            animator.SetBool("IsDead",true);
+            animator.SetBool("IsDead", true);
         }
     }
+
+    IEnumerator Respawn()
+    {
+
+        GameObject reSpawnText = GameObject.Find("RespawnText");
+        float respawnTime = 8.0f;
+        while (respawnTime > 0.0f)
+        {
+            yield return new WaitForSeconds(1.0f);
+            respawnTime -= 1.0f;
+            transform.GetComponent<PlayerMovementController>().enabled = false;
+            reSpawnText.GetComponent<Text>().text = "you are killed.Responding at:" + respawnTime.ToString(".00");
+        }
+
+
+        animator.SetBool("IsDead", false);
+        reSpawnText.GetComponent<Text>().text = "";
+
+        int radomPoint = Random.Range(-20, 20);
+        float randomPoint = 0;
+        transform.position = new Vector3(randomPoint, 0, randomPoint);
+        transform.GetComponent<PlayerMovementController>().enabled = true;
+        photonView.RPC("RegainHealth", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    public void RegainHealth()
+    {
+        health = startHealth;
+        healthBar.fillAmount = health / startHealth;
+    }
 }
+
